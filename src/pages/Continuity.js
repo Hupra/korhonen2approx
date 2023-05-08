0e2553a633089004cce2b0a700a4076ff9cd00a8
Sebastian Lundsgaard-Larsen <s_elementos@hotmail.com>
2023-05-08 13:48:10 +0200

    added text to continuity page

--- src/pages/Continuity.js
+++ src/pages/Continuity.js
@@ -49,10 +49,11 @@ function Continuity() {
         <div className='sidebar'><div className='sidebar_bubble'><SB style={{ height: '100vh', width: '100vw' }}>
             <h2>Continuity Issue</h2>
             <h1>Working on this at the moment</h1>
-            <p>When building the new tree decomposition T’ it must still follow the 3 rules for a tree decomposition. But if the separator X contains vertices that are outside of W, T’ can break rule 3 that says:</p>
+            <p>When building the new tree decomposition T’, it must still follow the 3 rules for a tree decomposition. But if the separator X contains vertices that are outside of W, T’ can break rule 3 that says:</p>
             <p> For every vertex <InlineMath math="v"/> contained in both bags <InlineMath math="B_i"/> and <InlineMath math="B_j"/>, the path in <InlineMath math="T"/> going from <InlineMath math="B_i"/> to <InlineMath math="B_j"/> must be of only bags containing <InlineMath math="v"/>.
             </p>
-            <p>Below you can click the different buttons to highlight the bags containing that vertex, to see if they are connected.</p>
+            <p>Below, you can click the different buttons to highlight the bags containing that vertex to see if they are connected.</p>
+
             <div className='button-box-5'>
                 <button onClick={() => button_handler(1)} className={selected===1 ? "error" : ""}>1 <ion-icon name="checkmark-outline" /></button>
                 <button onClick={() => button_handler(2)} className={selected===2 ? "error" : ""}>2 <ion-icon name="checkmark-outline" /></button>
@@ -67,6 +68,8 @@ function Continuity() {
                 <button onClick={() => button_handler(9)} className={selected===9 ? "error disable" : "disable"}>9 <ion-icon name="close-outline" /></button>
                 <button onClick={() => button_handler(10)} className={selected===10 ? "error disable" : "disable"}>10 <ion-icon name="close-outline" /></button>
             </div>
+            
+            <p style={{marginTop: "13px"}}>After clicking all the buttons, it should become evident that bags in <InlineMath math="T'"/> containing vertices 9 or 10 are not connected, thus violating rule 3 for tree decompositions. We will refer to this as the continuity issue, and the following pages will explore a method to resolve it, beginning with the introduction of home bags.</p>
             <hr/>
         <Link to="/homebag" className='button'>Continue <ion-icon name="arrow-forward-outline"></ion-icon></Link>
         <br/><i>Next: Home Bag</i>
